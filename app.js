869feedd43fcac6c1ea43a33f52af05d2d54d4b0
Ricardo Vazquez <ricardo@mozillafoundation.org>
2016-02-26 13:29:28 -0500

    Making it all work

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
   $('#default').click(function(event) {
+    event.preventDefault();
     $('.container').attr('id', 'default');
   });
   $('#navy').click(function(event) {
+    event.preventDefault();
     $('.container').attr('id', 'navy');
   });
+  $('#green').click(function(event) {
+    event.preventDefault();
+    $('.container').attr('id', 'green');
+  });
+  $('#teal').click(function(event) {
+    event.preventDefault();
+    $('.container').attr('id', 'teal');
+  });
+  $('#magenta').click(function(event) {
+    event.preventDefault();
+    $('.container').attr('id', 'magenta');
+  });
 });
-
-
-
-
-// btn-default
-// default
-// navy
-// green
-// teal
-// magenta
